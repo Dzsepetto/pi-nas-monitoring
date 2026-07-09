f1b6282c3721d9b873a70edf0d6b055409cc3f9f
DzsepettoConti <159457705+DzsepettoConti@users.noreply.github.com>
2026-07-09 14:21:04 +0200

    env update

--- pi-admin/src/api/apiClient.js
+++ pi-admin/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import { getToken } from '../utils/authStorage'
 
-const API_BASE_URL = 'http://100.109.109.54'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function apiRequest(path, options = {}) {
   const token = getToken()
